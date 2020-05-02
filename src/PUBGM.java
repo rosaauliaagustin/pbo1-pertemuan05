8eeb6ac786d2e6fb3e22f26318398635e1c51019
muhammad <dinoganteng7700@gmail.com>
2020-05-02 16:29:39 +0800

    PUBGM

---
+++ src/PUBGM.java
@@ -0,0 +1,16 @@
+public class PUBGM {
+
+    //attribute
+    String Username;
+    int score;
+    double length;
+
+    //behavior
+    void Winnerwinnerchickendinner(){
+        System.out.println("Fokus on the fire!!!");
+    }
+
+    void BattleGrounds(PUBGM PUBGM) {
+        score += 1/100;
+    }
+}
